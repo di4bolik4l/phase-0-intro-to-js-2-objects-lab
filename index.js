ef0d9ad7cfdb48e1b7aed31961f22c5af457f9f0
Justin Woodruff <justin.dwayne.woodruff@gmail.com>
2022-09-15 18:34:41 -0500

    commit tree b21c9d278cc15b12d7e9b4a473f97b826ec38187

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "Justin Woodruff",
+    streetAddress: "1600 Pennsylvania Ave.",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {
+        ...employee, 
+        ...{[key]: value}
+    };
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee2 = {...employee[key]};
+    delete newEmployee2[key]
+    return newEmployee2;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee;
+};
